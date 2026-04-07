a744c30e9d3ddba0d28d46ba0d6d20ca0c2cb326
FranchescaDelaRosaFDGDR <delarosa.fg@obmontessori.edu.ph>
2026-04-07 13:19:30 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// Username and Password
+function username_password () {
+    let username = document.getElementById('input1').value;
+    let password = document.getElementById('input2').value;
+
+    if (username === 'Franchesca Dela Rosa' && password === 'OBMCGH') {
+        window.alert('You have successfully logged in! Welcome, Franchesca Dela Rosa!');
+    } else {
+        window.alert('Incorrect username and/or password. Please try again.');
+    }
+}
+
+// Instructions
+function toggle_instructions() {
+    document.getElementById('toggle').innerHTML = 'Kindly fill up the following details. After doing so, click the login button to successfully log in to your account. If the password and/or username is incorrect, please try again.';
+}
